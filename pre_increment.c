a3802fe7a1543d7f97f268cac992be132857ff32
Rumman954 <116155649+Rumman954@users.noreply.github.com>
2025-02-22 18:48:34 +0600

    Add files via upload

---
+++ pre_increment.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int apple=0, orange=0;
+    printf("Initially You Have Apple = %d and Orange %d\nAdd Apple:",apple, orange);
+    scanf("%d", &apple);
+    printf("Now You Have: \nApple = %d Orange = %d\n", apple, orange);
+    orange = ++apple;
+    printf("::After Pre Increment::\n Now You Have: \nApple = %d  Orange = %d\n", apple, orange);
+
+return 0;
+}
